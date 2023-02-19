5b60ed0fc170c69c19ab7793ee2758c702eceac1
Dylan Watterson <dylanwatterson@Dylans-MBP.lan>
2023-02-19 18:34:58 +1100

    troubleshoot

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ port = os.environ['PORT']
 db = os.environ['DB']
 pw = os.environ['PW']
 
+db_uri = os.environ['DATABASE_URI']
+
 api_key = os.environ['API_KEY']
 api_secret = os.environ['API_SECRET']
 ax_token = os.environ['ACCESS_TOKEN']
 ax_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
-print(user, host, port, db, pw)
+print(user, host, port, db, pw, db_uri)
 
 
 # user, server, port, db, schema = 'postgres', 'localhost', '5432', 'postgres', 'coins'
@@ -23,20 +25,20 @@ print(user, host, port, db, pw)
 #     creds = json.loads(j.read())
 # user, host, port, db, pw = creds['user'],  creds['host'], creds['port'], creds['db'], creds['password']
 
-schema, table = 'coins', 'cmc_base_recently_added'
-conn = db_connection(user=user, host=host, port=port, db=db, cred=pw)
-
-
-df = cmc_recently_added()
-
-if __name__ == '__main__':
-    execute_tweet(df=df,
-                  schema=schema,
-                  table=table,
-                  conn=conn,
-                  akey=api_key,
-                  asecret=api_secret,
-                  axtoken=ax_token,
-                  axsecret=ax_token_secret
-                  )
+# schema, table = 'coins', 'cmc_base_recently_added'
+# conn = db_connection(user=user, host=host, port=port, db=db, cred=pw)
+#
+#
+# df = cmc_recently_added()
+#
+# if __name__ == '__main__':
+#     execute_tweet(df=df,
+#                   schema=schema,
+#                   table=table,
+#                   conn=conn,
+#                   akey=api_key,
+#                   asecret=api_secret,
+#                   axtoken=ax_token,
+#                   axsecret=ax_token_secret
+#                   )
 
