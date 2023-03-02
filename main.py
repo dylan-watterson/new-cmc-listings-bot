03b2a22a857079d95496afe772ae181c00d48f3a
Dylan Watterson <dylanwatterson@Dylans-MBP.lan>
2023-03-02 12:13:02 +1100

    changed api token / uncommented execute

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ conn = db_connection(user=user, host=host, port=port, db=db, cred=pw)
 
 df = cmc_recently_added()
 
-# if __name__ == '__main__':
-#     execute_tweet(df=df,
-#                   schema=schema,
-#                   table=table,
-#                   conn=conn,
-#                   akey=api_key,
-#                   asecret=api_secret,
-#                   axtoken=ax_token,
-#                   axsecret=ax_token_secret
-#                   )
+if __name__ == '__main__':
+    execute_tweet(df=df,
+                  schema=schema,
+                  table=table,
+                  conn=conn,
+                  akey=api_key,
+                  asecret=api_secret,
+                  axtoken=ax_token,
+                  axsecret=ax_token_secret
+                  )
