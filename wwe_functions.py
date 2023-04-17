c6157bc6279f6f70f00dee8dfe0cbc8e319159dd
Dylan Watterson <dylanwatterson@Dylans-MBP.lan>
2023-04-17 16:45:55 +1000

    added wwe bot

--- wwe_functions.py
+++ wwe_functions.py
@@ -87,7 +87,7 @@ def web_crawler(path, options):
     """
 
     # Create a WebDriver instance
-    chrome_options = webdriver.Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
